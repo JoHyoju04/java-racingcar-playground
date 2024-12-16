29bb2c71c20806b8c8c4346b564bec0321ee3113
JoHyoju04 <whgywn369@gmail.com>
2024-12-16 19:58:22 +0900

    test : 커스텀 문자열과 요구사항에 해당하는 테스트 코드 추가

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,6 +4,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StringCalculatorTest {
+    @Test
+    void 빈_문자열이면_0_반환(){
+        StringCalculator calculator = new StringCalculator();
+        assertThat(calculator.calculate("")).isEqualTo(0);
+    }
+
+    @Test
+    void null이면_0_반환(){
+        StringCalculator calculator = new StringCalculator();
+        assertThat(calculator.calculate(null)).isEqualTo(0);
+    }
 
     @Test
     void 쉼표_구분자를_기준으로_분리해서_더한다(){
@@ -30,6 +41,19 @@ class StringCalculatorTest {
     void 문자가_들어오면_예외(){
         StringCalculator calculator = new StringCalculator();
         assertThatThrownBy(() -> calculator.calculate("ab:,")).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 음수가_들어오면_예외(){
+        StringCalculator calculator = new StringCalculator();
+        assertThatThrownBy(() -> calculator.calculate("1,2:-3")).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 커스텀_구분자가_들어오면_계산(){
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.calculate("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
     }
 
 }
